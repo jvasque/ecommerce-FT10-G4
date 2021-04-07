ea8fdcc1b67b4d8cb593983d6a27e3897c7a1595
efedefede <63814886+efedefede@users.noreply.github.com>
2021-04-07 14:29:13 -0300

    S24 GET products/:id
    that route is succesfully done

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -11,4 +11,20 @@ router.get('/', (req, res, next) => {
 	res.json({product: 'Confirmado product'})
 });
 
+router.get('/:id', async(req, res, next) => {
+	var code = req.params.id;
+	var product = await Product.findOne({
+		where:{
+			productId: code
+		}
+	})
+    if (product){
+        res.status(200)
+		res.json(product)
+	}else{
+		res.status(400)
+		res.send("that product cannot be find")
+	}
+});
+
 module.exports = router;
