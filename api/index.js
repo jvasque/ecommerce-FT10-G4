1e97003473414d0d4c0747e8b0bfee482f8d952f
Dandreagus <73973939+Dandreagus@users.noreply.github.com>
2021-04-07 00:24:43 -0300

    ruta allCategories

--- api/index.js
+++ api/index.js
@@ -21,11 +21,8 @@ const server = require("./src/app.js");
 const { conn } = require("./src/db.js");
 
 // Syncing all the models at once.
-/* conn.sync({ force: true }).then(() => {
-  server.listen(3000, () => {
-    console.log('%s listening at 3000'); // eslint-disable-line no-console
+conn.sync({ force: true }).then(() => {
+  server.listen(3001, () => {
+    console.log("%s listening at 3000"); // eslint-disable-line no-console
   });
-}); */
-server.listen(3001, () => {
-  console.log("Server up");
 });
--- api/src/routes/categories/allCategories.js
+++ api/src/routes/categories/allCategories.js
@@ -1,9 +1,15 @@
 const { Router } = require("express");
+const { Category } = require("../../db.js");
 
 const allCategories = Router();
 
-allCategories.get("/", (req, res) => {
-  res.send("all Categories");
+allCategories.get("/", async (req, res) => {
+  await Category.create({
+    name: "Fertilizante",
+  });
+
+  const data = await Category.findAll();
+  res.json(data);
 });
 
 module.exports = allCategories;
