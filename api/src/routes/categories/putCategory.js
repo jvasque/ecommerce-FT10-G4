14f69d41b3fc16e933ce09bc05005e602ba74470
NataN <vasquezvalle@hotmail.com>
2021-04-07 12:43:35 -0500

    added logic putCategory

--- api/src/routes/categories/putCategory.js
+++ api/src/routes/categories/putCategory.js
@@ -1,8 +1,21 @@
 const { Router } = require("express");
+const { Category} = require("../../db.js");
 
 const putCategory = Router();
 
-putCategory.get("/", (req, res) => {
+putCategory.post("/", async (req, res) => {
+
+
+ let {newName, name} = req.body
+
+  let category = await Category.findOne({where:{
+     name:name
+  }});
+
+  category.name = newName 
+  await category.save();
+
+  console.log(req)
   res.send("PutCategory");
 });
 
