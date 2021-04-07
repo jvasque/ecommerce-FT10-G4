6c257c4d652726e298f13d5f84e759c66724c8f0
Dandreagus <73973939+Dandreagus@users.noreply.github.com>
2021-04-07 13:19:44 -0300

    Rutas delete y add funcionando

--- api/src/routes/categories/addCategory.js
+++ api/src/routes/categories/addCategory.js
@@ -1,8 +1,21 @@
 const add = require("express").Router();
-const { Product } = require("../../db.js");
+const { Category } = require("../../db.js");
 
-add.get("/", (req, res, next) => {
-  res.send("Add Category");
+add.post("/", async (req, res, next) => {
+  const { name } = req.body;
+  const findDuplicate = await Category.findAll({
+    where: {
+      name: name,
+    },
+  });
+  if (findDuplicate.length !== 0) {
+    res.send("Ya existe esa categoria");
+  } else {
+    await Category.create({
+      name: name,
+    });
+    res.send("Categoria Creada").status(200);
+  }
 });
 
 module.exports = add;
--- api/src/routes/categories/deleteCategory.js
+++ api/src/routes/categories/deleteCategory.js
@@ -1,9 +1,16 @@
 const { Router } = require("express");
+const { Category } = require("../../db.js");
 
 const deleteCategory = Router();
 
-deleteCategory.get("/", (req, res) => {
-  res.send("DELETE");
+deleteCategory.post("/", async (req, res) => {
+  const { name } = req.body;
+  await Category.destroy({
+    where: {
+      name: name,
+    },
+  });
+  res.send("DELETED");
 });
 
 module.exports = deleteCategory;
