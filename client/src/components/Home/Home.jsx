7dc1cc9a8b831d65f499f29553e3798e0761df7b
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-04-07 15:46:49 -0400

    Create Home.jsx

---
+++ client/src/components/Home/Home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../scss/components/_Home.scss";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <div className="grupo4">Kappa</div>
+    </div>
+  );
+};
+
+export default Home;
