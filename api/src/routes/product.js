29382f7b926110ed4f11eea90d0bef7a4218a290
efedefede <63814886+efedefede@users.noreply.github.com>
2021-04-07 23:16:15 -0300

    S26 PUT /products/:id prototype
    we almost are there!!

--- api/src/routes/product.js
+++ api/src/routes/product.js
@@ -28,13 +28,13 @@ router.get('/:id', async(req, res, next) => {
 		return res.json(product)
 	}else{
 		res.status(400)
-		return res.send("that product cannot be find")
+		return res.json({error: "that product cannot be find"})
 	}
 });
 
 router.post('/', async (req, res) => {
 
-	let { name, SKU, unitPrice, description, picture, score } = req.body;
+	let { name, SKU, unitPrice, description, picture, score, unitsOnStock } = req.body;
 
 	try{
 		const addProduct = await Product.findOrCreate({
@@ -44,7 +44,8 @@ router.post('/', async (req, res) => {
 				unitPrice,
 				description,
 				picture,
-				score
+				score,
+				unitsOnStock
 			}
 		})
 
@@ -55,14 +56,38 @@ router.post('/', async (req, res) => {
 				unitPrice,
 				description,
 				picture,
-				score
+				score,
+				unitsOnStock
 			}
 		})
 	
 		return res.json(findProduct)
 	} catch(err){
 		console.log(err);
-		res.json({err: "an error occurred while loading the data"})
+		res.json({error: "an error occurred while loading the data"})
 	}
 })
+
+router.put('/:id', async(req, res, next) => {
+	let {name, SKU, unitPrice, description, unitsOnStock} = req.body;
+	let code = req.params.id;
+	const product = await Product.findOne({
+		where:{
+			productId: code
+		}
+	})
+    if (product){
+		if(name) await product.update({name: name})
+		if(SKU) await product.update({SKU: SKU})
+		if(unitPrice) await product.update({unitPrice: unitPrice})
+		if(description) await product.update({description: description})
+		if(unitsOnStock) await product.update({unitsOnStock: unitsOnStock})
+
+        res.status(200)
+		return res.json(product)
+	}else{
+		res.status(400)
+		return res.json({error: "that product cannot be find"})
+	}
+});
 module.exports = router;
