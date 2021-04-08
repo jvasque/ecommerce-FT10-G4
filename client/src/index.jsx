c5088d42a520325b8d1d3cb377f2ade29f3c0af5
jearizaa <jearizaa@unal.edu.co>
2021-04-08 18:21:37 -0500

    Corrigiendo
    merge prueba 1

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './scss/_index.scss';
+import './_index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { Provider } from "react-redux";
