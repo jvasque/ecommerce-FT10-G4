af3b0513e331be9181618072fc1184637e95a5f3
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-04-08 20:14:53 -0400

    Commit about SCSS Home
    Modified SCSS Home,
    Change JSX for JS for APP and Index
    Create JSON for BD

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import "../../scss/components/_Home.scss";
-import Footer from "../Footer/Footer.jsx"
+
 
 const Home = () => {
   return (
     <div className="container">
-    
+   
       <form>
-        <input type="radio" name="fancy" autofocus value="clubs" id="clubs" />
+        <input type="radio" name="fancy" autoFocus value="clubs" id="clubs" />
         <input type="radio" name="fancy" value="hearts" id="hearts" />
         <input type="radio" name="fancy" value="spades" id="spades" />
         <input type="radio" name="fancy" value="diamonds" id="diamonds" />
-        <label for="clubs"> Fertilizantes</label><label for="hearts"> Insumos</label><label for="spades"> Categoria3</label><label for="diamonds"> Categoria4</label>
-        <div class="keys">Use arrows to navigate</div>
+        <label htmlFor="clubs"> Fertilizantes</label><label htmlFor="hearts"> Insumos</label><label htmlFor="spades"> Categoria3</label><label htmlFor="diamonds"> Categoria4</label>
+        <div className="keys">Use arrows to navigate</div>
       </form>
-
-
-     
     
     </div>
   );
