78c115b325d232be0157063cf6788b7e147bee64
barakdev <61583502+barakdev@users.noreply.github.com>
2021-04-10 04:00:57 -0300

    NavBar in Process
    alta lista desplegable

--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -2,32 +2,45 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "../../scss/components/_Nav.scss";
 // npm install --save-dev @iconify/react @iconify-icons/mdi
-import { Icon, InlineIcon } from '@iconify/react';
-import loginIcon from '@iconify-icons/mdi/login';
-
+import { Icon, InlineIcon } from "@iconify/react";
+import loginIcon from "@iconify-icons/mdi/login";
 
 const Nav = () => {
-  
-  return <div className="Nav">
-<Link to='/'><img id='logo' src="https://cdn.discordapp.com/attachments/828719452828860436/830295466481549332/logo-mercagro.png" alt=''/></Link>
-<div className="search-box">
-          <form >
-            <input
-           
-              type="search"
-              
-              
-              placeholder="Buscar..."
+  return (
+    <div className="Nav">
+      <Link>
+        <img
+          id="logo"
+          src="https://cdn.discordapp.com/attachments/828719452828860436/830295466481549332/logo-mercagro.png"
+          alt=""
+        />
+      </Link>
+      <div className="dropdown">
+        <ul className="hList">
           
-            />
-            <span className="searchspan"></span>
-            </form>
-</div>
-<div>
-<Icon icon={loginIcon} height='70' />
-</div>
-
-  </div>;
+            <a href="/catalog" className="menu">
+              <h2 className="menu-title">Productos</h2>
+              <ul className="menu-dropdown">
+                <li href="/catalog">Proteccion de Cultivos</li>
+                <li href="/catalog">Fertilizantes</li>
+                <li href="/catalog">Semillas e Hibridos</li>
+                <li href="/catalog">Otros Insumos Agricolas</li>                
+              </ul>
+            </a>
+                    
+        </ul>
+      </div>
+      <div className="search-box">
+        <form>
+          <input type="search" placeholder="Buscar..." />
+          <span className="searchspan"></span>
+        </form>
+      </div>
+      {/* <div>
+        <Icon icon={loginIcon} height="70" />
+      </div> */}
+    </div>
+  );
 };
 
 export default Nav;
