b9f6a897116fae4150da049458e186cbf67ad1f4
jearizaa <jearizaa@unal.edu.co>
2021-04-10 16:59:49 -0500

    Added routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,9 +3,10 @@ const { Router } = require("express");
 const addCategory = require("./categories/addCategory");
 const allCategories = require("./categories/allCategories");
 const deleteCategory = require("./categories/deleteCategory");
-const info = require("./categories/info");
 const putCategory = require("./categories/putCategory");
-const product = require("./product")
+const products = require("./products/product");
+const search = require('./search');
+
 const router = Router();
 
 // load each router on a route
@@ -15,7 +16,7 @@ router.use("/addCategory", addCategory);
 router.use("/deleteCategory", deleteCategory);
 router.use("/putCategory", putCategory);
 router.use("/allCategories", allCategories);
-router.use("/info", info);
-router.use("/products", product)
+router.use('/products', products);
+router.use('/search', search);
 
 module.exports = router;
