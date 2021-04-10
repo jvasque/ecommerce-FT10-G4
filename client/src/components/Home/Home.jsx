2aa1805de42d82021ca9f12ba244d938e6dec30f
jearizaa <jearizaa@unal.edu.co>
2021-04-10 16:26:31 -0500

    Conflicts Home

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,26 +1,19 @@
 import React from "react";
 import "../../scss/components/_Home.scss";
-
-
+import Footer from "../Footer/Footer.jsx"
 
 const Home = () => {
   return (
     <div className="container">
-    
       <form>
         <input type="radio" name="fancy" autofocus value="clubs" id="clubs" />
         <input type="radio" name="fancy" value="hearts" id="hearts" />
         <input type="radio" name="fancy" value="spades" id="spades" />
         <input type="radio" name="fancy" value="diamonds" id="diamonds" />
         <label for="clubs"> Fertilizantes</label><label for="hearts"> Insumos</label><label for="spades"> Categoria3</label><label for="diamonds"> Categoria4</label>
-        <div class="keys">Use arrows to navigate</div>
-      </form>
-      
-
-     
-    
+        <div className="keys">Use arrows to navigate</div>
+      </form> 
     </div>
-    
   );
 };
 
