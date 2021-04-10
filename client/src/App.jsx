7318680bd1263c0e24d67e5a0a69d330ef941590
jearizaa <jearizaa@unal.edu.co>
2021-04-10 17:28:41 -0500

    App from Quality

---
+++ client/src/App.jsx
@@ -0,0 +1,30 @@
+import React, { Fragment } from "react";
+import { Route, Switch } from "react-router";
+
+import Home from "./components/Home/Home";
+import Nav from "./components/Nav/Nav";
+import ProductDetails from "./components/ProductDetails/ProductDetails";
+import Catalog from './components/Catalog/Catalog.jsx'
+import Form from "./components/formCategories/Form";
+import "./scss/_App.scss";
+import Footer from "./components/Footer/Footer";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Fragment>
+        <Route path="/" render={() => <Nav />} />
+        <Route exact path="/" component={Home} />
+        <Route exact path="/categorias" component={Form} />
+        <Switch>
+          <Route path='/catalog' component={Catalog}/>
+          <Route exact path="/:id" component={ProductDetails} />
+        </Switch>
+        <Route path='/' component={ Footer }/>  
+      </Fragment>
+    </div>
+  );
+}
+
+export default App;
