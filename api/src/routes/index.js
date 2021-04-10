89843b22a5415fb37baab1c6f6622d1d891a71c9
jearizaa <jearizaa@unal.edu.co>
2021-04-10 16:12:18 -0500

    Index conflict routes

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,10 +1,10 @@
-const { Router } = require('express');
+const { Router } = require("express");
 // import all routers;
-const addCategory = require('./categories/addCategory');
-const allCategories = require('./categories/allCategories');
-const deleteCategory = require('./categories/deleteCategory');
-const putCategory = require('./categories/putCategory');
-const products = require('./product');
+const addCategory = require("./categories/addCategory");
+const allCategories = require("./categories/allCategories");
+const deleteCategory = require("./categories/deleteCategory");
+const putCategory = require("./categories/putCategory");
+const products = require("./products/product");
 const search = require('./search');
 
 const router = Router();
@@ -12,10 +12,10 @@ const router = Router();
 // load each router on a route
 // i.e: router.use('/auth', authRouter);
 // router.use('/auth', authRouter);
-router.use('/addCategory', addCategory);
-router.use('/deleteCategory', deleteCategory);
-router.use('/putCategory', putCategory);
-router.use('/allCategories', allCategories);
+router.use("/addCategory", addCategory);
+router.use("/deleteCategory", deleteCategory);
+router.use("/putCategory", putCategory);
+router.use("/allCategories", allCategories);
 router.use('/products', products);
 router.use('/search', search);
 
