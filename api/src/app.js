cb360e7f971fdd12bcb7958f41ebf17f0b59607f
jearizaa <jearizaa@unal.edu.co>
2021-04-10 16:01:03 -0500

    Conflicts app api

--- api/src/app.js
+++ api/src/app.js
@@ -1,31 +1,30 @@
-const express = require('express');
-const cookieParser = require('cookie-parser');
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const index = require('./routes/index.js');
-const cors = require('cors')
-require('./db.js');
+const express = require("express");
+const cookieParser = require("cookie-parser");
+const bodyParser = require("body-parser");
+const morgan = require("morgan");
+const index = require("./routes/index.js");
+
+require("./db.js");
 
 const server = express();
 
-server.name = 'API';
-server.use(cors())
+server.name = "API";
 
-server.use(express.urlencoded({ extended: true, limit: '50mb' }));
-server.use(express.json({ limit: '50mb' }));
+server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
+server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use(morgan('dev'));
+server.use(morgan("dev"));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3000'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Credentials", "true");
   res.header(
-    'Access-Control-Allow-Headers',
-    'Origin, X-Requested-With, Content-Type, Accept'
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
 });
 
-server.use('/', index);
+server.use("/", index); //Fixear
 
 // Error catching endware.
 server.use((err, req, res, next) => {
