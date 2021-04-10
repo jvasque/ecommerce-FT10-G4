881715653a4c5798776aff7dd1abd97a62b3beb6
jearizaa <jearizaa@unal.edu.co>
2021-04-10 17:07:07 -0500

    Changes from quality

--- client/src/App.js
+++ client/src/App.js
@@ -4,22 +4,24 @@ import { Route, Switch } from "react-router";
 import Home from "./components/Home/Home";
 import Nav from "./components/Nav/Nav";
 import ProductDetails from "./components/ProductDetails/ProductDetails";
-
-// import "./App.css";
+import Catalog from './components/Catalog/Catalog.jsx'
+import Form from "./components/formCategories/Form";
 import "./scss/_App.scss";
 import Footer from "./components/Footer/Footer";
+
 
 function App() {
   return (
     <div className="App">
       <Fragment>
-        <Route path="/" render={() => <Nav />} />        
+        <Route path="/" render={() => <Nav />} />
         <Route exact path="/" component={Home} />
+        <Route exact path="/categorias" component={Form} />
         <Switch>
+          <Route path='/catalog' component={Catalog}/>
           <Route exact path="/:id" component={ProductDetails} />
         </Switch>
-
-        <Route path='/' component={ Footer }/>
+        <Route path='/' component={ Footer }/>  
       </Fragment>
     </div>
   );
