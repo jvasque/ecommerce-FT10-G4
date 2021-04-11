86640865ce3517b30ffebbd795ec3d23dcf5f823
Patricio <contacto@patriciogatti.ar>
2021-04-11 19:35:05 -0300

    Catalog styles

--- client/src/components/Catalog/Pages.jsx
+++ client/src/components/Catalog/Pages.jsx
@@ -4,7 +4,7 @@ import '../../scss/components/Catalog/_Pages.scss'
 function Pages({totalProducts}){
     return (
         // <div>{`< 1 2 3 4 5 6 7 8 9 10 >`}</div>
-        <div>{`Se cargaron ${totalProducts} productos.`}</div>
+        <div className="catalogResults">{`Se cargaron ${totalProducts} productos.`}</div>
     )
 }
 
