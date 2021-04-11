6b3b825df59420bb056548578abe9339682bd131
jearizaa <jearizaa@unal.edu.co>
2021-04-11 17:00:46 -0500

    Icon Cart
    Checkbox for the card Cart icon

--- client/src/components/ProductCard/ButtonIconText.jsx
+++ client/src/components/ProductCard/ButtonIconText.jsx
@@ -1,22 +1,26 @@
 import React from 'react'
 import '../../scss/components/ProductCard/_ButtonIconText.scss'
 import DivText from './DivText.jsx'
-import { BiCartAlt } from "react-icons/bi";
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import Checkbox from '@material-ui/core/Checkbox';
 import Favorite from '@material-ui/icons/Favorite';
 import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
 
 const Heart = (<FormControlLabel
     control={<Checkbox icon={<FavoriteBorder />} checkedIcon={<Favorite />} name="checkedH" />}
+/>)
+
+const Cart = (<FormControlLabel
+    control={<Checkbox icon={<ShoppingCartOutlinedIcon />} checkedIcon={<ShoppingCartIcon style={{color: "white"}}/>} name="checkedC" />}
 />)
 
 function ButtonIconText(props){
     return (
         <div className='buttonIcon'>
             <div className='iconContainer'>
-                {/* {props.icon === 'Heart' ? <BiHeart className='icon'/>:<BiCartAlt className='icon'/>} */}  
-                {props.icon === 'Heart' ? Heart:<BiCartAlt className='icon'/>}              
+                {props.icon === 'Heart' ? Heart:Cart}              
             </div>
             <div className='textContainer'>
                 <DivText className='textIcon' content={props.icon === 'Heart' ? 'Add to Favorites':'Add to Cart'}/>
