5976cf7733327041d29400bedb091db32ad23a9c
Carlangas11 <73184352+Carlangas11@users.noreply.github.com>
2021-04-13 18:51:45 -0400

    commit redux

---
+++ client/src/redux/postUserReducer/postUserReducer.js
@@ -0,0 +1,15 @@
+import { POST_USER } from "./postUserActions";
+
+
+
+ const postUserReducer = (state, action) => {
+
+  switch(action.type) {
+    case POST_USER:
+      return 
+    
+  }
+}
+
+
+export default postUserReducer
--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -4,6 +4,7 @@ import detailReducer from './detailReducer/detailReducer';
 import searchReducer from './searchReducer/searchReducer'
 import catalogReducer from './catalogReducer/catalogReducer'
 import categoryFilterReducer from './categoryFilterReducer/categoryFilterReducer'
+
  
 const rootReducer = combineReducers({
     detailReducer,
@@ -11,6 +12,7 @@ const rootReducer = combineReducers({
     catalogReducer,
     categoryFilterReducer,
     reducerProductForms,
+    
 });
 
 export default rootReducer;
