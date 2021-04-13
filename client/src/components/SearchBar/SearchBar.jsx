a8dff518de7f5a08879dc8eed92628d0acbc15c2
Pía Cherni <73494684+piacz@users.noreply.github.com>
2021-04-12 23:12:06 -0300

    Started Autocomplete
    Started the autocomplet of the searchbar

--- client/src/components/SearchBar/SearchBar.jsx
+++ client/src/components/SearchBar/SearchBar.jsx
@@ -3,25 +3,31 @@ import { useHistory } from "react-router";
 import '../../scss/components/_SearchBar.scss'
 import { useDispatch, useSelector} from "react-redux";
 import { BiSearch } from "react-icons/bi";
-import { getQuery, resetQuery } from '../../redux/searchReducer/searchActions';
+import { getQuery, resetQuery, getOptions, resetOptions } from '../../redux/searchReducer/searchActions';
 import { filterCategory } from '../../redux/categoryFilterReducer/categoryFilterActions';
 
 
 function SearchBar() {
-  const [find, setFind] = useState('');  
+  const [find, setFind] = useState('');
+
   const query = useSelector(state => state.searchReducer.query)
   const findQuery = useSelector(state => state.searchReducer.findQuery)
+  const options = useSelector(state => state.searchReducer.options)
+  
+
   const dispatch = useDispatch();
   const history = useHistory();
   
   useEffect(() => {  
     setFind(findQuery)
     return
-  },[dispatch, query, findQuery])
+  },[dispatch, query, findQuery ])
 
   const handleChange = (e) => {
     setFind(e.target.value);
-    if(e.target.value === '') dispatch(resetQuery())    
+    if(e.target.value === '') dispatch(resetQuery())
+    if(e.target.value === '') dispatch(resetOptions())
+    if(e.target.value.length > 1){dispatch(getOptions(e.target.value))}
   };
 
   const handleSubmit = (e) => {
@@ -34,24 +40,38 @@ function SearchBar() {
   };
 
   return (
-    <div className='searchBar'>
-      <form className='searchBarForm'
-        onSubmit={handleSubmit}
-      >
-        <input id="search"
-          type="search"
-          placeholder="Search..."
-          autoFocus required
-          value={find}
-          autocomplete="off"
-          onChange={handleChange} />
-        <button type="submit"><BiSearch /></button>
-      </form>
+    <div>
+      <div className='searchBar'>
+        <form className='searchBarForm'
+          onSubmit={handleSubmit}
+        >
+          <input id="search"
+            type="search"
+            placeholder="Search..."
+            autoFocus required
+            value={find}
+            autoComplete="off"
+            onChange={handleChange} />
+          <button type="submit"><BiSearch /></button>
+        </form>
+      </div>
+
+      <div className="displayOptions">
+        <ul>
+        {options?.map( (result, i) => (
+          <li className='optionsLi' key={i}>
+            {result.name}
+          </li>
+          )
+        )}
+        </ul>
+      </div>
+      
     </div>
   )
 }
 
 export default SearchBar;
-//busca del array loaded por titulo (ruta query por keyword de busqueda)
+
 
 
