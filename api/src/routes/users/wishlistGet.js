e6612da5bd272480b21bd5527681bce0f0d85524
Patricio <contacto@patriciogatti.ar>
2021-04-14 20:28:58 -0300

    wishlist GET update #3

---
+++ api/src/routes/users/wishlistGet.js
@@ -0,0 +1,47 @@
+// users/:id/wishlist/:name
+//already set for marketplace
+
+const { Wishlist, Product, User } = require('../../db.js');
+
+module.exports = async (req, res, next) => {
+  let id = req.params.id;
+  let name = req.params.name;
+
+  try {
+    let data = await User.findOne({
+      where: {
+        userId: id,
+      },
+      attributes: ['firstName', 'lastName'],
+      include: {
+        model: Wishlist,
+        attributes: ['name'],
+        where: {
+          name: name,
+        },
+        through: {
+          attributes: [],
+        },
+        include: {
+          model: Product,
+          attributes: [
+            'name',
+            'SKU',
+            'unitPrice',
+            'description',
+            'picture',
+            'score',
+            'unitsOnStock',
+          ],
+          through: {
+            attributes: [],
+          },
+        },
+      },
+    });
+    return res.json(data);
+  } catch (err) {
+    res.json(err);
+    return console.log(err);
+  }
+};
