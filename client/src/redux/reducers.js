d8cb5dedb86b45ac0983328992ab6425970677c6
Patricio <contacto@patriciogatti.ar>
2021-04-13 23:06:09 -0300

    Fix great merge

--- client/src/redux/reducers.js
+++ client/src/redux/reducers.js
@@ -1,18 +1,20 @@
 import { combineReducers } from 'redux';
 import reducerProductForms from './reducerProductForms/reducerProductForms';
 import detailReducer from './detailReducer/detailReducer';
-import searchReducer from './searchReducer/searchReducer'
-import catalogReducer from './catalogReducer/catalogReducer'
-import categoryFilterReducer from './categoryFilterReducer/categoryFilterReducer'
-import iconReducer from './iconReducer/iconReducer'
+import searchReducer from './searchReducer/searchReducer';
+import catalogReducer from './catalogReducer/catalogReducer';
+import categoryFilterReducer from './categoryFilterReducer/categoryFilterReducer';
+import iconReducer from './iconReducer/iconReducer';
+import cartReducer from './cartReducer/cartReducer';
 
 const rootReducer = combineReducers({
-    detailReducer,
-    searchReducer,
-    catalogReducer,
-    categoryFilterReducer,
-    reducerProductForms,
-    iconReducer,
+  detailReducer,
+  searchReducer,
+  catalogReducer,
+  categoryFilterReducer,
+  reducerProductForms,
+  iconReducer,
+  cartReducer,
 });
 
 export default rootReducer;
