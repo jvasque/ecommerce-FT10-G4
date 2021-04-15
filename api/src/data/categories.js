54d4e94e65743e37cc4b287da79b6dc3d8f3a0d1
Patricio <contacto@patriciogatti.ar>
2021-04-15 15:15:25 -0300

    Data updated

--- api/src/data/categories.js
+++ api/src/data/categories.js
@@ -1,18 +1,18 @@
 module.exports = [
-    {
-      name: "Proteccion de Cultivos",
-      subcategorySubcategoryId: [1, 2, 3, 4],
-    },
-    {
-      name: "Fertilizantes",
-      subcategorySubcategoryId: [5, 6, 7, 8],
-    },
-    {
-      name: "Otros Insumos Agricolas",
-      subcategorySubcategoryId: [9, 10],
-    },
-    {
-      name: "Semillas e Hibridos",
-      subcategorySubcategoryId: [11, 12, 13, 14, 15],
-    },
-  ]
+  {
+    name: 'Proteccion de Cultivos',
+    subId: [1, 2, 3, 4],
+  },
+  {
+    name: 'Fertilizantes',
+    subId: [5, 6, 7, 8],
+  },
+  {
+    name: 'Otros Insumos Agricolas',
+    subId: [9, 10],
+  },
+  {
+    name: 'Semillas e Hibridos',
+    subId: [11, 12, 13, 14, 15],
+  },
+];
