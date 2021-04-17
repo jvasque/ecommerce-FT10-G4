9585021ae489fb40a765cb7148fd69ff44aa183f
Patricio <contacto@patriciogatti.ar>
2021-04-17 20:45:34 -0300

    Menu updated

--- client/src/components/Nav/AdminMenu.jsx
+++ client/src/components/Nav/AdminMenu.jsx
@@ -1,59 +1,72 @@
-import "../../scss/components/Nav/_AdminMenu.scss"
-import {Link} from "react-router-dom"
-import {BiLogOut, BiListPlus} from "react-icons/bi"
-import {FaProductHunt} from "react-icons/fa"
-import {RiAccountCircleLine} from "react-icons/ri"
+import '../../scss/components/Nav/_AdminMenu.scss';
+import { Link } from 'react-router-dom';
+// import { BiLogOut, BiListPlus } from 'react-icons/bi';
+// import { FaProductHunt } from 'react-icons/fa';
+import { RiAccountCircleLine, RiShoppingCart2Line } from 'react-icons/ri';
 import ShoppingCartIcon from '@material-ui/icons/ShoppingCart';
-import React,{useState} from 'react'
-import { useDispatch, useSelector } from "react-redux";
-import { LogOut } from "../../redux/loginReducer/loginActions";
-
-
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { LogOut } from '../../redux/loginReducer/loginActions';
 
 export default function AdminMenu() {
-    const [colorChange, setColorChange] = useState("")
-    const products = useSelector(state => state.cartReducer.cart)
-    let productCart = products.length !== 0 ? products.length : ""
+  const [colorChange, setColorChange] = useState('');
+  const products = useSelector((state) => state.cartReducer.cart);
 
-    const dispatch = useDispatch()
-    const log = useSelector(state => state.loginReducer)
-   
-    const handleLog = () => {
-        setColorChange("Exit")
-        if(log.isLogin) {
-            dispatch(LogOut());
-            alert("deslogeado")
-        }
-        
-    }
-    return (
-        <div>
+  let productCart = products.length !== 0 ? products.length : '';
 
-        <div className="adminMenu">
-            <ul>
-                <li>
-                    <Link className="test" to="/user/info" style={colorChange === "User" ? {color:"rgba(243, 208, 11, 0.87)"} : {color :""}} 
-                    onClick={() => setColorChange("User")}><RiAccountCircleLine/></Link>
-                </li>
-                <li>
+  //   const dispatch = useDispatch();
+
+  //   const handleLogOut = () => {
+  //     setColorChange('Exit');
+  //     if (log.isLogin) {
+  //       dispatch(LogOut());
+  //       alert('deslogeado');
+  //     }
+  //   };
+
+  return (
+    <div>
+      <div className="adminMenu">
+        <ul>
+          <li>
+            <Link
+              className="test"
+              to="/user/info"
+              style={
+                colorChange === 'User'
+                  ? { color: 'rgba(243, 208, 11, 0.87)' }
+                  : { color: '' }
+              }
+              onClick={() => setColorChange('User')}
+            >
+              <RiAccountCircleLine />
+            </Link>
+          </li>
+          {/* <li>
                     <Link className="test" to="/admin/product/form" style={colorChange === "Product" ? {color:"rgba(243, 208, 11, 0.87)"} : {color :""}} 
                     onClick={() => setColorChange("Product")}><FaProductHunt/></Link>
-                </li>
-                <li>
+                </li> */}
+          {/* <li>
                     <Link className="test" to="/admin/categories" style={colorChange === "Categories" ? {color:"rgba(243, 208, 11, 0.87)"} : {color :""}}
                     onClick={() => setColorChange("Categories")}><BiListPlus/></Link>
-                </li>
-                <li>
-                    <Link className="test" style={colorChange === "Exit" ? {color:"rgba(243, 208, 11, 0.87)"} : {color :""}}  to={log.isLogin?"/":'/user/login'} 
-                    onClick={handleLog}><BiLogOut/></Link>
-        
-                </li>
-                
-             
-                
-            </ul>
-        </div>
-            <Link className="cart-items" to="/product/cart"><ShoppingCartIcon/>{productCart}</Link>
-        </div>
-    )
+                </li> */}
+          <li>
+            <Link
+              className="test"
+              to="/product/cart"
+              style={
+                colorChange === 'Cart'
+                  ? { color: 'rgba(243, 208, 11, 0.87)' }
+                  : { color: '' }
+              }
+              onClick={() => setColorChange('Cart')}
+            >
+              <RiShoppingCart2Line />
+              {productCart}
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
