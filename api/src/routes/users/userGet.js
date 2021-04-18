be7013321366ead0001bdfb37ad511e7bcd4aba1
Patricio <contacto@patriciogatti.ar>
2021-04-18 18:34:23 -0300

    Merge security

--- api/src/routes/users/userGet.js
+++ api/src/routes/users/userGet.js
@@ -2,35 +2,6 @@ const { User, Product, Order, OrderDetail } = require('../../db.js');
 
 module.exports = async (req, res) => {
   try {
-<<<<<<< HEAD
-
-      let data = await User.findAll({
-        include: {
-          model: Order,
-          include: [
-            {
-              model: OrderDetail,
-              include: Product,
-            },
-          ],
-        },
-      });
-      const users = data
-        .map((d) => d.dataValues)
-        .sort(function (a, b) {
-          if (a.id > b.id) {
-            return 1;
-          }
-          if (a.id < b.id) {
-            return -1;
-          }
-          return 0;
-        });
-      return res.json(users);
-   
-    }
-   catch (err) {
-=======
     let data = await User.findAll({
       include: {
         model: Order,
@@ -55,7 +26,6 @@ module.exports = async (req, res) => {
       });
     return res.json(users);
   } catch (err) {
->>>>>>> Quality
     res.json(err);
     return console.log(err);
   }
--- client/src/redux/loginReducer/loginReducer.js
+++ client/src/redux/loginReducer/loginReducer.js
@@ -1,6 +1,5 @@
-import { LOGIN_ACTION_KEY, LOG_FAIL, LOG_OUT, LOG_SWAL } from "./loginActions";
-import decode from "jwt-decode";
-
+import { LOGIN_ACTION_KEY, LOG_FAIL, LOG_OUT, LOG_SWAL } from './loginActions';
+import decode from 'jwt-decode';
 
 const initialState = {
   user: {},
@@ -8,40 +7,37 @@ const initialState = {
   errorLogin: false,
   isLogin: false,
   isAdmin: false,
- 
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
     case LOGIN_ACTION_KEY:
-      
-   
       return {
         ...state,
         user: decode(action.payload),
         isLogin: true,
         isAdmin: action.payload.type.includes('admin'),
-        errorLogin:  false,
-        error:{}
+        errorLogin: false,
+        error: {},
       };
-      case LOG_FAIL:
-        return{
-          ...state,
-          errorLogin:  true,
-          error:action.payload
-        }
+    case LOG_FAIL:
+      return {
+        ...state,
+        errorLogin: true,
+        error: action.payload,
+      };
     case LOG_OUT:
-      localStorage.removeItem("token");
+      localStorage.removeItem('token');
       return {
         ...state,
         user: {},
         isLogin: false,
         isAdmin: false,
       };
-      case LOG_SWAL:
-        return {
-          errorLogin:false
-        }
+    case LOG_SWAL:
+      return {
+        errorLogin: false,
+      };
     default:
       return { ...state };
   }
