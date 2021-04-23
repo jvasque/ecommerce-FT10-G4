64d95848522440ca67a82b1a0c76a3eb763e2e96
efedefede <63814886+efedefede@users.noreply.github.com>
2021-04-23 20:30:34 -0300

    Mail order notification HTML layout fixed

--- api/src/routes/emails/emailOrderNotification.js
+++ api/src/routes/emails/emailOrderNotification.js
@@ -98,91 +98,111 @@ module.exports = async (req, res) => {
   }
 
   let htmlCreator = `
-  <head>
-      <style>
-          .email-container {
-    background-color: rgba(239, 239, 239, 1);
-    color: rgba(42, 46, 42, 1);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-  }
-  .header {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: rgb(48, 126, 19);
-    color: rgb(224, 192, 8);
-    width: 100%;
-  }
-  .banner {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    background-color: rgba(42, 46, 42, 1);
-    color: rgba(239, 239, 239, 1);
-    width: 100%;
-    margin-top: 1%;
-    padding-top: 2%;
-    padding-bottom: 2%;
-  }
-  
-  .details {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin: 3%;
-    height: 300px;
-    width: 400px;
-    background-color: rgba(105, 104, 104, 0.322);
-    color: rgba(42, 46, 42, 1);
-  }
-  .detItem {
-    margin: 1%;
-  }
-  .image {
-    margin: 4%;
-    height: 40%;
-    width: 40%;
-    background-color: rgb(224, 192, 8);
-    color: rgba(105, 104, 104, 1);
-  }
-  footer {
-    background-color: rgba(42, 46, 42, 1);
-    color: rgba(239, 239, 239, 1);
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 2%;
-  }
-      </style>
-      <title>Document</title>
-  </head>
-  <body>
-      <div class="email-container">
-          <div class="header">
-             <h1>Agroplace</h1> 
-          </div>
-          <div class="banner">
-              Hola ${firstName}! Tu paquete llegará entre el ${dayOftheWeek(day1.getUTCDay())} ${day1.getUTCDate()} de ${monthOfTheYear(day1.getUTCMonth())} y el ${dayOftheWeek(day2.getUTCDay())} ${day2.getUTCDate()} de ${monthOfTheYear(day2.getUTCMonth())}
-          </div>
-          <div class="body-email-container">
-              
-              <div class="details">
-                  Detalle de envio:
-                  <div class="image">${"Imagen" || img}</div>
-                  <div class="detItem">${"Direccion" || Direccion}</div>
-                  <div class="detItem">${"Producto" || producto}</div>
-                  <div class="detItem">${"Link" || linkaalgunlado}</div>
-              </div>
-          </div>
-          </div>
-          <footer>
-              <h4>Titulo</h4>
-              <div>Necesitas ayuda? Contactanos</div>
-              <div>${"Algun dato nuestro linkeado" || nustrosdatoslinkeados}</div>
-          </footer>
-  </body>
+  <html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <style>
+        .email-container {
+  background-color: rgba(239, 239, 239, 1);
+  color: rgba(42, 46, 42, 1);
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  text-align: center;
+}
+.header {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: rgb(48, 126, 19);
+  color: rgb(224, 192, 8);
+  width: 100%;
+  text-align: center;
+}
+.banner {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  background-color: rgba(42, 46, 42, 1);
+  color: rgba(239, 239, 239, 1);
+  width: 100%;
+  margin-top: 1%;
+  padding-top: 2%;
+  padding-bottom: 2%;
+  text-align: center;
+}
+
+.details {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 3%;
+  margin-bottom: 3%;
+  height: 300px;
+  width: 400px;
+  background-color: rgba(105, 104, 104, 0.322);
+  color: rgba(42, 46, 42, 1);
+  text-align: center;
+}
+.detItem {
+    display: block;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 1%;
+  text-align: center;
+}
+.image {
+    margin-left: auto;
+  margin-right: auto;
+  margin-top: 4%;
+  margin-bottom: 4%;
+  height: 40%;
+  width: 40%;
+  background-color: rgb(224, 192, 8);
+  color: rgba(105, 104, 104, 1);
+}
+footer {
+  background-color: rgba(42, 46, 42, 1);
+  color: rgba(239, 239, 239, 1);
+  text-align: center;
+  margin-left: auto;
+  margin-right: auto;
+  padding-left: auto;
+  padding-right: auto;
+  padding-bottom: 2%;
+}
+
+    </style>
+    <title>Document</title>
+</head>
+<body>
+    <div class="email-container">
+        <div class="header">
+           <h1>Agroplace</h1> 
+        </div>
+        <div class="banner">
+            <p>Hola ${firstName}! Tu paquete llegará entre el ${dayOftheWeek(day1.getUTCDay())} ${day1.getUTCDate()} de ${monthOfTheYear(day1.getUTCMonth())} y el ${dayOftheWeek(day2.getUTCDay())} ${day2.getUTCDate()} de ${monthOfTheYear(day2.getUTCMonth())}</p>
+        </div>
+        <div class="body-email-container">
+            
+            <div class="details">
+                Detalle de envio:
+                <div class="image">${"Imagen" || img}</div>
+                <div class="detItem">${"Direccion" || Direccion}</div>
+                <div class="detItem">${"Producto" || producto}</div>
+                <div class="detItem">${"Link" || linkaalgunlado}</div>
+            </div>
+        </div>
+        </div>
+        <footer>
+            <h4>Titulo</h4>
+            <div>Necesitas ayuda? Contactanos</div>
+            <div>${"Algun dato nuestro linkeado" || nustrosdatoslinkeados}</div>
+        </footer>
+</body>
+</html>
     `;
 
   let mailOptions = {
