4d055cb5099015d37b7a2716b5e7e5f1b85003e8
jearizaa <jearizaa@unal.edu.co>
2021-04-24 12:46:38 -0500

    MapRoute

--- client/src/components/LocationStock/MapContainer.jsx
+++ client/src/components/LocationStock/MapContainer.jsx
@@ -7,12 +7,12 @@ export default function MapContainer(props) {
         city: '',
         country: '',
     })
-    const [addressUrl, setAddressUrl] = useState(address.address.replace(' ', '+'))
-    const [cityUrl, setCityUrl] = useState(address.city.replace(' ', '+'))
-    const [countryUrl, setCountryUrl] = useState(address.country.replace(' ', '+'))
-    let origin = `KR+83+65C+18,Bogota,Colombia`
-    let destination = `KR+101+70+14,Bogota,Colombia`
-    let waypoints = `Kr91+100`
+    const [addressUrl, setAddressUrl] = useState({
+        address: address.address.replace(' ', '+'),
+        city: address.city.replace(' ', '+'),
+        country: address.country.replace(' ', '+'),
+    })
+
     function handleSubmit(event){
         event.preventDefault()
 
@@ -21,34 +21,30 @@ export default function MapContainer(props) {
     function handleChange(event){
         let {name, value} = event.target        
         setAddress({...address, [name]: value})
-        if(name === 'address'){setAddressUrl(value.replace(/[^\w\s]/gi, '').split(' ').join('+'))}
-        if(name === 'city'){setCityUrl(value.replace(/[^\w\s]/gi, '').split(' ').join('+'))}
-        if(name === 'country'){setCountryUrl(value.replace(/[^\w\s]/gi, '').split(' ').join('+'))}
-
+        setAddressUrl({...addressUrl, [name]: value.replace(/[^\w\s]/gi, '').split(' ').join('+')})
     }
 
     return (
         <div>MapContainer
         <form onSubmit={handleSubmit}>
+        <label>Dirección:
             <input type='text' name='address' value={address.address} onChange={handleChange}/>
+        </label>
+        <label>Ciudad:
             <input type='text' name='city' value={address.city} onChange={handleChange}/>
+        </label>
+        <label>Pais:
             <input type='text' name='country' value={address.country} onChange={handleChange}/>
+        </label>
             <input type='submit'/>
         </form>
-        <Iframe url={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAWvV27eRWz99D9ILk_vL8ETJWj6LB0e-I&language=es&q=${addressUrl},${cityUrl},${countryUrl}`}
+        <Iframe url={`https://www.google.com/maps/embed/v1/place?key=AIzaSyAWvV27eRWz99D9ILk_vL8ETJWj6LB0e-I&language=es&q=${addressUrl.address},${addressUrl.city},${addressUrl.country}`}
             width="450px"
             height="450px"
             id="myId"
             className="myClassname"
             display="initial"
             position="relative"/> 
-        <Iframe url={`https://www.google.com/maps/embed/v1/directions?key=AIzaSyAWvV27eRWz99D9ILk_vL8ETJWj6LB0e-I&language=es&origin=${origin}&destination=${destination}&waypoints=${waypoints}`}
-            width="450px"
-            height="450px"
-            id="myId"
-            className="myClassname"
-            display="initial"
-            position="relative"/>        
         </div>
     )
 }
