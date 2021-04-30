149d3d24f7b66a7b9e67fe4ac0d372f0c1017a73
efedefede <63814886+efedefede@users.noreply.github.com>
2021-04-30 03:30:48 -0300

    PromotionsModify incomplete

--- api/src/routes/promotions/promotions.js
+++ api/src/routes/promotions/promotions.js
@@ -3,12 +3,14 @@ const promotionsGet = require("./promotionsGet");
 const promotionsPost = require("./promotionsPost");
 const promotionsPut = require("./promotionsPut");
 const promotionsDelete = require("./promotionsDelete");
+const promotionsGetId = require("./promotionsGetId");
 
 const router = require("express").Router();
 router.use(bodyParser.json());
 
 router.get("/", promotionsGet);
 router.post("/", promotionsPost);
+router.get("/:id", promotionsGetId);
 router.put("/:id", promotionsPut);
 router.delete("/:id", promotionsDelete);
 
--- client/src/components/PromotionsForm/PromotionsModify.jsx
+++ client/src/components/PromotionsForm/PromotionsModify.jsx
@@ -1,9 +1,12 @@
 import React from "react";
+import axios from "axios";
 import { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { NavLink } from "react-router-dom";
 import _PromotionsModify from "../../scss/components/PromotionsForm/_PromotionsModify.scss";
 import { makeStyles } from '@material-ui/core/styles';
+import { TextField } from '@material-ui/core';
+import CancelIcon from '@material-ui/icons/Cancel';
 import Button from '@material-ui/core/Button';
 
 const grisPrincipal= "#EFEFEF";
@@ -30,8 +33,24 @@ const useStyles = makeStyles((theme) => ({
   }));
 
 function PromotionsModify(props) {
+  const category = useSelector(
+    (state) => state.categoryFilterReducer.categories
+  );
   const dispatch = useDispatch();
   const classes = useStyles();
+  /* console.log(props.history.location.promotion, "MI PROMOCION ANTES") */
+const {
+  description,
+  categoryCheck,
+  products,
+  discountDate,
+  combo,
+  days,
+  active, 
+  id,
+} = props.history.location.promotion;
+
+  const [promotion, setPromotion] = useState([]);
   const [input, setInput] = useState({
     description: '',
     categoryCheck: [],
@@ -41,29 +60,119 @@ function PromotionsModify(props) {
     days: []
     
   });
-    console.log(props.history.location.promotion, "MI PROMOCION")
-  const {
-    active,
-    combo,
-    days,
-    description,
-    discountDate,
-    id,
-    products,
-    categoryCheck
-  } = props.history.location.promotion;
+  const handleChange = (e) => {
+    setInput({
+      ...input,
+      [e.target.name]: e.target.value,
+    });
+  };
+  
+  const handleCategoryCheck = function (e) {
+   if (e.target.checked) {
+      setInput({
+        ...input,
+        categoryCheck: [...input.categoryCheck, parseInt(e.target.value)],
+      });
+    } else {
+      setInput({
+        ...input,
+        categoryCheck: input.categoryCheck.filter(
+          (category) => category !== parseInt(e.target.value)
+        ),
+      });
+    }
+  };
+ /*  const handleSubmit = function (event) {
+    event.preventDefault();
+      dispatch(
+        postPromotion(
+          input.description,
+          input.categoryCheck,
+          input.products,
+          input.discountDate,
+          input.combo,
+          input.days
+        )
+      );
+      setInput({
+        description: '',
+        categoryCheck: [],
+        products: [],
+        discountDate: [],
+        combo: '',
+        days: []
+      });
+      
+      setProductRender([]);
+      setName('');
+      let inputs = document.querySelectorAll('input[type=checkbox]');
+      inputs.forEach((item) => {
+        item.checked = false;
+      });
+      swal('Éxito!',`La promocion ${input.combo} ha sido modificada`, 'success'); 
+  };  */
+
+  async function getPromotion() {
+    const token = localStorage.getItem("token");
+    const info = await axios.get("http://localhost:3001/promotions/"+ id, 
+    {
+      headers: { Authorization: `Bearer ${token}` },
+    });
+
+    setPromotion(info.data);
+  }
+  useEffect(() => {
+    setPromotion([]);
+    getPromotion();
+    
+  }, []);
+  console.log(promotion, "MI PROMO ESTADO")
    return (
     <div className="containerPromotionsModify">
-      <h1>Renderizado !</h1>
-      <p>Ver consola</p>
-      <h1>Active:</h1>
-      <p>{active.toString()}</p>
+      <form className={classes.root}  /* onSubmit={(e) => handleSubmit(e)} */ >
+       <h1>Modificar promociones</h1>
+       <div className="cont-2">
+
+       <TextField 
+          id="outlined-basic" 
+          label={promotion.description} 
+          placeholder="Agregue la nueva descripción de la promoción..."
+          variant="outlined"
+          name="description"
+          value={input.description} 
+          type="text"
+          required 
+          onChange={handleChange}
+          className={classes.input}/>
+
+         <label className="label">Categoria:</label>
+           <div className="categoryBoxes">
+             {category &&
+               category.map((c) => {
+                 let check = promotion.categoryCheck?.includes(c.id).toString()
+                 return ( 
+                   <div key={c.name}>
+                     <label>{c.name}</label>
+                     <input
+                      type="checkbox"
+                      value={c.id}
+                      onChange={(e) =>  handleCategoryCheck(e)}
+                      checked = { check }
+                      
+                     />
+                   </div>
+                 );
+               })}
+           </div>
+
+        
+      
       <h1>Combo:</h1>
       <p>{combo}</p>
       <h1>Días:</h1>
       <p>{days}</p>
-      <h1>Descripción:</h1>
-      <p>{description}</p>
+      {/* <h1>Descripción:</h1>
+      <p>{description}</p> */}
       <h1>Descuento:</h1>
       <p>{`${discountDate}%`}</p>
       <h1>Id:</h1>
@@ -72,6 +181,8 @@ function PromotionsModify(props) {
       <p>{products?.map(e => `${e.id}, `)}</p>
       <h1>Categories Id's:</h1>
       <p>{categoryCheck?.map(e => `${e}, `)}</p>
+       </div>
+      </form>
       <Button variant="contained" color="primary">
         <NavLink to="/user/info" style={{ textDecoration: 'none', color:"#eee" }}>Volver</NavLink>
       </Button>
