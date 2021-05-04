a89236277bd6187c750d661e7a47315b3b9fd61f
jearizaa <jearizaa@unal.edu.co>
2021-05-03 23:42:01 -0500

    Fixing Async calls

--- client/src/components/ProductCard/ProductCard.jsx
+++ client/src/components/ProductCard/ProductCard.jsx
@@ -6,49 +6,52 @@ import ButtonIconText from './ButtonIconText';
 import ScoreIcon from './ScoreIcon';
 
 function ProductCard({product}){
- 
-    return (
-        <div className='productCard'>
-            <Link className='cardLink' to={`/${product.id}`}>
-                <div className='cardPicture'>
-                    <img src={product.picture[0]} alt='product'></img>
-                </div>
-                <div className='cardContent'>
-
-                    <div className='cardData'>
-                        <div className='cardScore'>
-                            <div className='starIcon'>
-                                <ScoreIcon score={Math.round(product.score)}/>
-                            </div>                        
+    if(product){
+        return (
+            <div className='productCard'>
+                <Link className='cardLink' to={`/${product.id}`}>
+                    <div className='cardPicture'>
+                        <img src={product.picture[0]} alt='product'></img>
+                    </div>
+                    <div className='cardContent'>
+    
+                        <div className='cardData'>
+                            <div className='cardScore'>
+                                <div className='starIcon'>
+                                    <ScoreIcon score={Math.round(product.score)}/>
+                                </div>                        
+                            </div>
+                            <div className='cardPrice'>
+                                <DivText content={`USD$${product.unitPrice}`}/>
+                            </div>
                         </div>
-                        <div className='cardPrice'>
-                            <DivText content={`USD$${product.unitPrice}`}/>
+    
+                        <div className='cardText'>
+                            {/* <p id='nameCard'><b>{product.name}</b></p> */}
+                            {/* <p id='descriptionCard'>{product.description}</p> */}
+                            <b><DivText content={product.name}/></b>
                         </div>
                     </div>
-
-                    <div className='cardText'>
-                        {/* <p id='nameCard'><b>{product.name}</b></p> */}
-                        {/* <p id='descriptionCard'>{product.description}</p> */}
-                        <b><DivText content={product.name}/></b>
-                    </div>
+                </Link>
+                <div className='cardButtons'>
+                    <div className='wishlistButton'>
+                        <div className='cardIcon'>
+                            <ButtonIconText icon='Heart' productId={product.id}/>
+                        </div>
+                    </div>                     
+                    <div className='addCartButton'>   
+    
+                    {product.unitsOnStock !== 0 ? <div className='cardIcon'>
+                            <ButtonIconText product={product} productId={product.id}/>
+                        </div> : "" }
+                      
+                    </div>                     
                 </div>
-            </Link>
-            <div className='cardButtons'>
-                <div className='wishlistButton'>
-                    <div className='cardIcon'>
-                        <ButtonIconText icon='Heart' productId={product.id}/>
-                    </div>
-                </div>                     
-                <div className='addCartButton'>   
-
-                {product.unitsOnStock !== 0 ? <div className='cardIcon'>
-                        <ButtonIconText product={product} productId={product.id}/>
-                    </div> : "" }
-                  
-                </div>                     
             </div>
-        </div>
-    )
+        )
+    }else{
+        return <></>
+    }    
 } 
 
 export default ProductCard
